004ecc3dce599fb7eb97d0bc3bb2bdce572ce85c
Dale Price <daprice@mac.com>
2024-09-01 17:36:56 -0500

    Remove unused listener code

--- src/__init__.py
+++ src/__init__.py
@@ -205,32 +205,16 @@ def periodic_update():
     # print("ran periodic update")
     return 10.0
 
-# def receive_update(appearance):
-#     """Callback for dark mode switch listener"""
-#     if bpy.types.WindowManager.ADM_dark_mode_listening == True:
-#         bpy.ops.adm.update_theme()
-
 def start_watching():
     """Start watching for changes, either by starting polling or by listening for changes from the OS"""
     bpy.app.timers.register(periodic_update, persistent=True)
     # print("started periodic update")
     
-    # thread = threading.Thread(target=darkdetect.listener, args=(receive_update,))
-    # thread.daemon = True
-    # thread.start()
-    # 
-    # bpy.types.WindowManager.ADM_dark_mode_listening = True
-    # 
-    # bpy.ops.adm.update_theme()
-    
 def stop_watching():
     bpy.app.timers.unregister(periodic_update)
-    
-    # bpy.types.WindowManager.ADM_dark_mode_listening = False
 
 def register():
     bpy.types.WindowManager.ADM_dark_mode_active = None
-    # bpy.types.WindowManager.ADM_dark_mode_listening = None
     
     bpy.utils.register_class(ADMAutoDarkMode)
     bpy.utils.register_class(ADM_update_theme)
@@ -239,7 +223,7 @@ def register():
     bpy.utils.register_class(ADM_set_light_theme)
     bpy.utils.register_class(ADM_set_dark_theme)
     
-    bpy.app.timers.register(start_watching)
+    start_watching()
 
 def unregister():
     stop_watching()
